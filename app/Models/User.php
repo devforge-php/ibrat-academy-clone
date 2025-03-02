e982177faaac53e8cdbef50d6497c11779e50427
Azizbekweb098 <webcoderazizbek@gmail.com>
2025-03-02 22:01:45 +0500

    yakshanba

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+
         'email',
-        'password',
+
     ];
 
     /**
@@ -29,8 +29,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+    
     ];
 
     /**
@@ -39,7 +38,7 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
+   
+
     ];
 }
